b230b49eb38f1498fcd8c185ae65b23d257d95ec
g14121 <109278356+g14121@users.noreply.github.com>
2022-07-14 15:15:47 +0530

    Add files via upload

---
+++ continuebreak.java
@@ -0,0 +1,47 @@
+
+public class continuebreak {
+	public static void main(String[] args) {
+		
+			for (int i=1;i<=10;i++)
+		{
+			
+						if (i==5)
+			{
+									continue;
+			}
+			
+			
+			
+										System.out.println("value is" +i);
+			
+			
+		}
+		
+		
+		
+				for (int j=2;j<=10;j++)
+		{
+					
+					
+					
+								if(j>7)
+		{
+			
+			
+										break;
+		}
+		
+												System.out.println(j);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
